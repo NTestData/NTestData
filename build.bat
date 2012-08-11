e2a7f2cf84fcb4ebf6116fc54ee6ade673de98ca
Igor Toporet <igort@iqmetrix.com>
2012-08-10 17:34:13 -0700

    clean build all configurations

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
-@echo off
+@ECHO OFF
 
-set MSBuild=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+SET MSBuild=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 
-%MSBuild% NTestData.sln /t:Rebuild /v:m /nologo /p:Configuration=Release.Plus.NBuilder
+SET BuildConfigurations=Debug,Release,Release.Plus.AutoPoco,Release.Plus.NBuilder
+SET BuildTargets=Clean,Build
+SET VerbosityLevel=/v:minimal
+SET UseAllProcessors=/m
 
-pause
+FOR %%G IN (%BuildConfigurations%) DO (
+	%MSBuild% NTestData.sln /t:%BuildTargets%  /p:Configuration=%%G %VerbosityLevel% %UseAllProcessors% /nologo
+)
+
+PAUSE
