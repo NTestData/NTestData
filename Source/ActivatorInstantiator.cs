b276ca46f78e8e9337112035657979da4ce6fdc2
Igor Toporet <igort@iqmetrix.com>
2012-08-10 17:59:23 -0700

    documented IInstantiator & ActivatorInstantiator

--- Source/ActivatorInstantiator.cs
+++ Source/ActivatorInstantiator.cs
@@ -1,7 +1,16 @@
 namespace NTestData
 {
+    /// <summary>
+    /// Instantiates objects of specified types using <see cref="System.Activator"/>.
+    /// </summary>
     public class ActivatorInstantiator : IInstantiator
     {
+        /// <summary>
+        /// Creates instance of specified type <typeparamref name="T"/>
+        /// using <see cref="System.Activator.CreateInstance(System.Type)"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of object to be instantiated.</typeparam>
+        /// <returns>Instance of specified type <typeparamref name="T"/>.</returns>
         T IInstantiator.Instantiate<T>()
         {
             return (T) System.Activator.CreateInstance(typeof (T));
--- Source/IInstantiator.cs
+++ Source/IInstantiator.cs
@@ -1,7 +1,15 @@
 ﻿namespace NTestData
 {
+    /// <summary>
+    /// Instantiates objects of specified types.
+    /// </summary>
     public interface IInstantiator
     {
+        /// <summary>
+        /// Creates instance of specified type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of object to be instantiated.</typeparam>
+        /// <returns>Instance of specified type <typeparamref name="T"/>.</returns>
         T Instantiate<T>();
     }
 }
