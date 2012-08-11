1da8bc23c04646f8b78338bc4870e2d45a144bb7
Igor Toporet <igor.toporet@gmail.com>
2012-08-10 23:22:32 -0700

    samples project added

---
+++ Samples/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿namespace NTestData.Samples
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class HelloWorld
+    {
+        private class Greeting
+        {
+            public string Message { get; set; }
+        }
+
+        [Fact]
+        public void Demo()
+        {
+            var helloWorld = TestData.Create<Greeting>(Hello, World);
+
+            helloWorld.Message.Should().Be("Hello, World!");
+        }
+
+        private void Hello(Greeting greeting)
+        {
+            greeting.Message = "Hello, ";
+        }
+
+        private void World(Greeting greeting)
+        {
+            greeting.Message += "World!";
+        }
+    }
+}
